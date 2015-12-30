41a273b88930343db89b1a60425e9f0eb9c00ab9
Javier Ros <jros@emuasa.es>
2015-12-30 12:38:16 +0100

    Fix commit files

---
+++ Angular2Emulator/TypeScript/Ng2Emulation/Core/Bootstrap.ts
@@ -0,0 +1,30 @@
+﻿import {Angular1Wrapper} from "Ng2Emulation/Core/Angular1Wrapper"
+import ElementEvents from "Ng2Emulation/Events/ElementEvents"
+
+/**
+ * Class to store bootstrap information.
+ */
+export class BootStrapper {
+    private static services: Function[] = [];
+    static AddService(service: Function) {
+        this.services.push(service);
+    }
+    static BootStrap(component: Function, angular1DependendModules?: string[]) {
+
+        Angular1Wrapper.createModule(angular1DependendModules);
+
+        ElementEvents.resolve().forEach(directive => Angular1Wrapper.registerDirective(directive));
+        
+        Angular1Wrapper.registerServices(BootStrapper.services);
+        Angular1Wrapper.registerComponent(component);
+
+        Angular1Wrapper.bootstrap();
+    }
+}
+
+/**
+ * bootstrap function to init Angular.
+ */
+export function bootstrap(component: Function, angular1DependendModules?: string[]) {
+    BootStrapper.BootStrap(component, angular1DependendModules);
+}
