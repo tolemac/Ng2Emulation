f0ec4ccdf4a4decd40ee8008f4d345a4419a14d4
Javier Ros <tolemac@jros.org>
2016-01-02 01:02:22 +0100

    The Hero Editor Ok!

--- Angular2Emulator/TypeScript/App/app.component.ts
+++ Angular2Emulator/TypeScript/App/app.component.ts
@@ -1,8 +1,25 @@
 ﻿import {Component} from "Ng2Emulation/Ng2Emulation";
 
+export interface Hero {
+	id: number;
+	name: string;
+}
 @Component({
 	selector: "my-app",
-	template: "<h1>My First Angular 2 App</h1>"
+	template: `
+    <h1>{{title}}</h1>
+    <h2>{{hero.name}} details!</h2>
+    <div><label>id: </label>{{hero.id}}</div>
+    <div>
+      <label>name: </label>
+      <div><input [(ngModel)]="hero.name" placeholder="name"></div>
+    </div>
+    `
 })
 export class AppComponent {
+	public title = "Tour of Heroes";
+	public hero: Hero = {
+		id: 1,
+		name: "Windstorm"
+	};
 }
