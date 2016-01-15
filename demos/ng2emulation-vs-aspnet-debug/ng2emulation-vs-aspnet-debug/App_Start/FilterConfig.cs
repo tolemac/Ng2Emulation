0b29f2f8b4e2ad550c4cfe0c31fc07da2fe7d231
Javier Ros <tolemac@jros.org>
2016-01-15 03:12:39 +0100

    Add debug project

---
+++ demos/ng2emulation-vs-aspnet-debug/ng2emulation-vs-aspnet-debug/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace ng2emulation_vs_aspnet_debug
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ demos/ng2emulation-vs-aspnet-debug/ng2emulation-vs-aspnet-debug/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ng2emulation_vs_aspnet_debug.MvcApplication" Language="C#" %>
